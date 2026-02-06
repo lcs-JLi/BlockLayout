6fa013856bc3919e2977ba0f855a5a6191949828
John Li <jli@lcs.on.ca>
2026-02-06 09:00:06 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Xichen Li on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
