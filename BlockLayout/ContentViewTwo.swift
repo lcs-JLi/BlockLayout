22ce96ffcf0bc2c135ca00c18504e9daf56e9bb3
John Li <jli@lcs.on.ca>
2026-02-06 09:55:51 -0500

    Compeleted second Block layout

---
+++ BlockLayout/ContentViewTwo.swift
@@ -0,0 +1,19 @@
+//
+//  ContentViewTwo.swift
+//  BlockLayout
+//
+//  Created by Xichen Li on 2/6/26.
+//
+
+import SwiftUI
+
+struct ContentViewTwo: View {
+    var body: some View {
+        VStack{
+            Rectangle()
+        }
+    }
+}
+#Preview {
+    ContentViewTwo()
+}
